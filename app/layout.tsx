b064c7ee3ae22c05133ee9db49143437d9af1cb8
Ridwan O. Abdullateef <49338477+readone97@users.noreply.github.com>
2025-06-04 14:07:26 +0100

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,88 @@
+// import type React from "react"
+// import type { Metadata } from "next"
+// import { Inter } from "next/font/google"
+// import  AmigoWalletProvider  from "@/providers/AmigoWalletProvider"
+
+// import "./globals.css"
+// import { ThemeProvider } from "@/components/theme-provider"
+// import { AuthProvider } from "@/context/auth-context"
+// import { CryptoPriceProvider } from "react-realtime-crypto-prices"
+
+// const inter = Inter({ subsets: ["latin"] })
+
+// export const metadata: Metadata = {
+//   title: "Amigo Exchange",
+//   description: "Seamless Crypto to Fiat Exchange Platform",
+//     generator: 'v0.dev',
+    
+// }
+
+
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode
+// }>) {
+//   return (
+//     <html lang="en" suppressHydrationWarning>
+//       <body className={inter.className}>
+   
+//         <AuthProvider>
+//           <AmigoWalletProvider>
+//             <ThemeProvider defaultTheme="dark" enableSystem>
+//               {children}
+//             </ThemeProvider>
+//           </AmigoWalletProvider>
+//         </AuthProvider>
+    
+//       </body>
+//     </html>
+//   )
+// }
+
+
+import type React from "react"
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
+import { ThemeProvider } from "@/components/theme-provider"
+import './globals.css'
+import AmigoWalletProvider from "@/providers/AmigoWalletProvider"
+import { Providers } from "./provider";
+import { AccountProvider } from "@/context/AccountContext"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: "Amigo Exchange",
+  description: "Seamless Crypto to Fiat Exchange Platform",
+    generator: 'v0.dev'
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <AccountProvider>
+          <AmigoWalletProvider>
+          <ThemeProvider defaultTheme="dark" enableSystem>
+          {children}
+        </ThemeProvider>
+        </AmigoWalletProvider>
+
+        </AccountProvider>
+        
+      </body>
+    </html>
+  )
+}
+
+
+
+
+
+
